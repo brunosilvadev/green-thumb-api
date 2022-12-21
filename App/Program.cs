e195a2b12ce1f28d86c1944f8b0af997a571a783
Bruno Silva <brunosilva.dev@gmail.com>
2022-12-21 16:51:12 -0600

    added SQLite infrastructure

--- App/Program.cs
+++ App/Program.cs
@@ -1,13 +1,29 @@
+using GreenThumb.Persistence;
+using GreenThumb.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddSqlite<PlantDbContext>("Data Source=plants.db");
+builder.Services.AddTransient<IDatabaseService, PlantDatabaseService>();
+
 var app = builder.Build();
 
 app.UseSwagger();
 
-GreenThumb.Model.Plant plant = new();
+Plant plant = new();
 app.MapGet("/test", () => "Hello World!");
 app.MapGet("/plant-name", plant.Name);
+app.MapPut("/add-plant", async (IDatabaseService svc, Plant plant) =>
+{
+    await svc.AddPlant(plant);
+});
+app.MapGet("list-plants", async (IDatabaseService svc) =>
+{
+    return await svc.GetPlants();
+});
 
 app.UseSwaggerUI(options =>
 {
